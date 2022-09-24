4b1d544328b245e6f15ac485e9374b686fc4fc38
Elijah Odjegba <elijahodjegba@gmail.com>
2022-09-24 02:37:15 +0100

    Style the navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,34 +1,19 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const Navbar = () =>
-// const links = [
-//   {
-//     id: 1,
-//     path: '/',
-//     text: 'Books',
-//   },
-//   {
-//     id: 2,
-//     path: '/categories',
-//     text: 'Categories',
-//   },
-// ];
-
-  (
-    <header>
-      <h1>Bookstore CMS</h1>
-      <nav>
-
-        <ul>
-          <li>
-            <Link to="/Book">Book</Link>
-          </li>
-          <li>
-            <Link to="/Categories">Categories</Link>
-          </li>
-        </ul>
-      </nav>
-    </header>
-  );
+const Navbar = () => (
+  <header>
+    <h1>Bookstore CMS</h1>
+    <nav>
+      <ul>
+        <li>
+          <Link to="/Book">Book</Link>
+        </li>
+        <li>
+          <Link to="/Categories">Categories</Link>
+        </li>
+      </ul>
+    </nav>
+  </header>
+);
 export default Navbar;
